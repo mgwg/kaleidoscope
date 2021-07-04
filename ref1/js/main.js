cb3b61dd5ba0ed73705e7ebe72b40c2567587488
mgwg <wangmaggief@gmail.com>
2021-07-04 10:09:06 -0400

    deleted stuff

--- ref1/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".kal_cont").each(function(i){ 
-        $(this).mousemove(function(e) {
-            $(this).find(".ksc").each(function(i){ 
-                $(this).css({backgroundPosition: e.pageX+"px "+e.pageY+"px"});
-            });
-        });
-    });
-});
